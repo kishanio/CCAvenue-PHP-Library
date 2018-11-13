4e198373c386ecc6b7936e85799a14dc4111c2a7
Devin Walker <sdtucson@gmail.com>
2018-11-13 14:03:43 -0800

    use classes $this->hextobin rather than PHPs

--- src/Utils.php
+++ src/Utils.php
@@ -86,7 +86,7 @@ class Utils {
 		$key = $this->hextobin(md5($key));
 		$initVector = pack("C*", 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f);
 		$openMode = openssl_encrypt($plainText, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $initVector);
-		$encryptedText = bin2hex($openMode);
+		$encryptedText = $this->hextobin($openMode);
 		return $encryptedText;
 	}
 
@@ -94,7 +94,7 @@ class Utils {
 	{
 		$key = $this->hextobin(md5($key));
 		$initVector = pack("C*", 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f);
-		$encryptedText = hex2bin($encryptedText);
+		$encryptedText = $this->hextobin($encryptedText);
 		$decryptedText = openssl_decrypt($encryptedText, 'AES-128-CBC', $key, OPENSSL_RAW_DATA, $initVector);
 		return $decryptedText;
 
